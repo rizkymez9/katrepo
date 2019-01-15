909349bced419be67ad696937f83f89fe19d7f52
rizkymez9 <rizkymez9@gmail.com>
2019-01-15 14:04:20 +0800

    user1 adding step to search image of katalon studio

---
+++ Object Repository/Page_katalon studio - Google Search/a_Images.rs
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Images</name>
+   <tag></tag>
+   <elementGuidId>d13549b6-5051-4726-bae6-2ee40e8dc063</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='hdtb-msb-vis']/div[3]/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>q qs</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/search?q=katalon+studio&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwiiqq6yju_fAhXaF3IKHbc1AwEQ_AUIDygC</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Images</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;hdtb-msb-vis&quot;)/div[@class=&quot;hdtb-mitem hdtb-imb&quot;]/a[@class=&quot;q qs&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <value>//div[@id='hdtb-msb-vis']/div[3]/a</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <value>//a[contains(text(),'Images')]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Videos'])[1]/following::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='All'])[1]/following::a[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='News'])[2]/preceding::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Maps'])[2]/preceding::a[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <value>//a[contains(@href, '/search?q=katalon+studio&amp;source=lnms&amp;tbm=isch&amp;sa=X&amp;ved=0ahUKEwiiqq6yju_fAhXaF3IKHbc1AwEQ_AUIDygC')]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <value>//div/div/div/div/div[3]/a</value>
+   </webElementXpaths>
+</WebElementEntity>
--- Scripts/Test case by user1/Script1547524142594.groovy
+++ Scripts/Test case by user1/Script1547524142594.groovy
@@ -13,3 +13,15 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('https://www.google.com')
+
+WebUI.navigateToUrl('https://www.google.com/')
+
+WebUI.setText(findTestObject('Object Repository/Page_Google/input_Sign in_q'), 'katalon studio')
+
+WebUI.navigateToUrl('https://www.google.com/search?source=hp&ei=Tnc9XKLyOMyAyAOshYnICw&q=katalon+studio&btnK=Google+Search&oq=katalon+studio&gs_l=psy-ab.3..0l10.3159.5927..8370...0.0..0.699.4336.1j5j2j2j1j3......0....1..gws-wiz.....0..0i131.64qdzs_BgpQ')
+
+WebUI.click(findTestObject('Object Repository/Page_katalon studio - Google Search/a_Images'))
+
+WebUI.closeBrowser()
+
